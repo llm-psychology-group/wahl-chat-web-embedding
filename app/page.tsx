1d31f3fd9f27d997a6750ce22704df1d0d0fe712
Anton Wyrowski <anton.wyrowski@gmail.com>
2025-01-29 14:00:00 +0000

    feat: phone embed example

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,42 @@
 'use client';
 
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 export default function Home() {
-  const [partyId, setPartyId] = useState('afd');
+  const [partyId, setPartyId] = useState<string[]>([]);
+
+  const link = useMemo(() => {
+    const url = new URL('https://wahl.chat/session');
+
+    if (partyId.length > 0) {
+      partyId.forEach((id) => {
+        if (!id) return;
+        url.searchParams.append('party_id', id);
+      });
+    }
+
+    return url.toString();
+  }, [partyId]);
+
+  const handlePartyIdChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedPartyIds = Array.from(
+      e.target.selectedOptions,
+      (option) => option.value
+    );
+    setPartyId(selectedPartyIds);
+  };
+
+  const parties = {
+    spd: 'SPD',
+    afd: 'AfD',
+    cdu: 'CDU',
+    gruene: 'Grüne',
+    volt: 'Volt',
+    linke: 'Linke',
+    bsw: 'BSW',
+    fdp: 'FDP',
+    '': 'wahl.chat',
+  };
 
   return (
     <div className="flex flex-col h-screen p-4">
@@ -11,37 +44,36 @@ export default function Home() {
         Implement wahl.chat on your website
       </h1>
 
-      <p className="text-sm text-gray-500">
-        wahl.chat is a chat widget that allows you to chat with your customers
-        on your website.
-      </p>
-
-      <div className="aspect-[9/19.5] w-full border-2 border-gray-300 rounded-lg overflow-hidden mx-auto mt-6">
-        <iframe
-          src={`https://wahl.chat/session?party_id=${partyId}`}
-          style={{ border: 'none' }}
-          className="size-full"
-        />
-      </div>
-
       <div className="flex flex-col gap-2 mt-6">
         <label htmlFor="partyId" className="text-sm font-bold">
-          Party ID
+          Party ID - Wähle bis zu 3 Parteien aus
         </label>
         <select
           id="partyId"
-          className="border-2 border-gray-300 rounded-lg p-2"
+          className="border-2 border-gray-300 rounded-lg p-2 w-fit"
           value={partyId}
-          onChange={(e) => setPartyId(e.target.value)}
+          onChange={handlePartyIdChange}
+          multiple
         >
-          <option value="spd">SPD</option>
-          <option value="afd">AfD</option>
-          <option value="cdu">CDU</option>
-          <option value="gruene">Grüne</option>
-          <option value="volt">Volt</option>
-          <option value="linke">Linke</option>
-          <option value="bsw">BSW</option>
+          {Object.entries(parties).map(([key, value]) => (
+            <option key={key} value={key}>
+              {value}
+            </option>
+          ))}
         </select>
+
+        <p className="text-sm text-gray-500">
+          iFrame URL: <code className="text-sm text-gray-700">{link}</code>
+        </p>
+      </div>
+
+      <div className="flex gap-2 py-6 grow flex-wrap md:flex-nowrap">
+        <div className="hidden md:block grow border-2 border-gray-300 rounded-lg overflow-hidden mx-auto mt-6">
+          <iframe src={link} style={{ border: 'none' }} className="size-full" />
+        </div>
+        <div className="aspect-[9/19.5] border-2 border-gray-300 rounded-lg overflow-hidden mx-auto mt-6">
+          <iframe src={link} style={{ border: 'none' }} className="size-full" />
+        </div>
       </div>
     </div>
   );
